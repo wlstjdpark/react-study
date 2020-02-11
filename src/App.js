b603766ebbdf4ffe0dce92954619178d922e713f
jinsung <wlstjdpark@gmail.com>
2020-02-12 01:45:45 +0900

    폴더 이동, chapter7

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
-import IterationSample from './IterationSample';
+import LifeCycleSample from './Chapter07/LifeCycleSample';
+import ErrorBoundary from './Chapter07/ErrorBoundary';
 import './App.css';
 
 const App = () => {
+  const [color, setColor] = React.useState('#000000');
+
+  const getRandomColor = () => {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+  };
+
+  const onClick = () => {
+    setColor(getRandomColor());
+  };
   return (
     <div>
-      <IterationSample></IterationSample>
+      <button onClick={onClick}>랜덤 색상</button>
+      <ErrorBoundary>
+        <LifeCycleSample color={color}></LifeCycleSample>
+      </ErrorBoundary>
     </div>
   );
 };
---
+++ src/Chapter07/LifeCycleSample.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+
+class LifeCycleSample extends Component {
+  state = {
+    number: 0,
+    color: null
+  };
+
+  myRef = null;
+
+  constructor(props) {
+    super(props);
+    console.log('constructor');
+  }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    console.log('getDerivedStateFromProps');
+    if (nextProps.color !== prevState.color) {
+      return { color: nextProps.color };
+    }
+    return null;
+  }
+
+  componentDidMount = () => {
+    console.log('componentDidMount');
+  };
+
+  shouldComponentUpdate = (nextProps, nextState) => {
+    console.log('shouldComponentUpdate', nextProps, nextState);
+    return nextState.number % 10 !== 4;
+  };
+
+  handleClick = () => {
+    this.setState({
+      number: this.state.number + 1
+    });
+  };
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('getSnapshotBeforeUpdate');
+    if (prevProps.color !== prevState.color) {
+      return this.myRef.style.color;
+    }
+    return null;
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log('componentDidUpdate', prevProps, prevState);
+    if (snapshot) {
+      console.log('color before update', snapshot);
+    }
+  }
+
+  render() {
+    console.log('render');
+    const style = {
+      color: this.props.color
+    };
+
+    return (
+      <div>
+        {/* 에러 발생용 */}
+        {this.props.props.props}
+        <h1 style={style} ref={ref => (this.myRef = ref)}>
+          {this.state.number}
+        </h1>
+        <p>color: {this.state.color}</p>
+        <button onClick={this.handleClick}>더하기</button>
+      </div>
+    );
+  }
+}
+
+export default LifeCycleSample;
