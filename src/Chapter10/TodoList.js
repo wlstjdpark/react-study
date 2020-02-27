cd84ed40de2410c91fb12e91febe235acfe065ed
jinsung <wlstjdpark@gmail.com>
2020-02-28 03:03:48 +0900

    Chapter11 - optimized

--- src/Chapter10/TodoList.js
+++ src/Chapter10/TodoList.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { memo, useCallback } from 'react';
+import { List } from 'react-virtualized';
 import ToDoListItem from './TodoListItem';
 import styled from 'styled-components';
 
-const List = styled.div`
+const MyList = styled(List)`
   min-height: 320px;
   max-height: 513px;
   overflow: auto;
 `;
 
 const TodoList = ({ todos, onRemove, onToggle }) => {
+  const rowRenderer = useCallback(
+    ({ index, key, style }) => {
+      const todo = todos[index];
+      return (
+        <ToDoListItem
+          todo={todo}
+          key={key}
+          onRemove={onRemove}
+          onToggle={onToggle}
+          style={style}
+        ></ToDoListItem>
+      );
+    },
+    [onRemove, onToggle, todos],
+  );
   return (
-    <List>
+    <MyList
+      width={512} // 전체 크기
+      height={513} //  전체 높이
+      rowCount={todos.length} // 항목 개수
+      rowHeight={57}
+      rowRenderer={rowRenderer} // 항목을 렌더링할 때 쓰는 함수
+      list={todos} // 배열
+      style={{ outline: 'none' }} // List에 기본 적용되는 outline 스타일 제거
+    >
       {todos &&
         todos.map(todo => (
           <ToDoListItem
@@ -20,8 +44,8 @@ const TodoList = ({ todos, onRemove, onToggle }) => {
             onToggle={onToggle}
           />
         ))}
-    </List>
+    </MyList>
   );
 };
 
-export default TodoList;
+export default memo(TodoList);
--- src/Chapter10/TodoListItem.js
+++ src/Chapter10/TodoListItem.js
@@ -7,68 +7,76 @@ import {
 import styled from 'styled-components';
 import cn from 'classnames';
 
-const ListItem = styled.div`
-  padding: 1rem;
-  display: flex;
-  align-items: center;
-  &:nth-child(even) {
-    background: #f8f9fa;
-  }
-  .checkbox {
-    cursor: pointer;
-    flex: 1;
-    display: flex;
-    align-items: center;
-    svg {
-      font-size: 1.5rem;
-    }
-    .text {
-      margin-left: 0.5rem;
-      flex: 1;
-    }
-    &.checked {
-      svg {
-        color: #22b8cf;
-      }
-      .text {
-        color: #adb5bd;
-        text-decoration: line-through;
-      }
-    }
-  }
-  .remove {
-    display: flex;
-    align-items: center;
-    font-size: 1.5rem;
-    color: #ff6b6b;
-    cursor: pointer;
-    &:hover {
-      color: #ff8787;
-    }
-  }
-
+const ListWrapper = styled.div`
   & + & {
     border-top: 1px solid #dee2e6;
   }
+
+  &:nth-child(even) {
+    background: #f8f9fa;
+  }
+
+  .list-item {
+    padding: 1rem;
+    display: flex;
+    align-items: center;
+    &:nth-child(even) {
+      background: #f8f9fa;
+    }
+    .checkbox {
+      cursor: pointer;
+      flex: 1;
+      display: flex;
+      align-items: center;
+      svg {
+        font-size: 1.5rem;
+      }
+      .text {
+        margin-left: 0.5rem;
+        flex: 1;
+      }
+      &.checked {
+        svg {
+          color: #22b8cf;
+        }
+        .text {
+          color: #adb5bd;
+          text-decoration: line-through;
+        }
+      }
+    }
+    .remove {
+      display: flex;
+      align-items: center;
+      font-size: 1.5rem;
+      color: #ff6b6b;
+      cursor: pointer;
+      &:hover {
+        color: #ff8787;
+      }
+    }
+  }
 `;
 
-const TodoListItem = ({ todo, onRemove, onToggle }) => {
+const TodoListItem = ({ todo, onRemove, onToggle, style }) => {
   const { id, text, checked } = todo;
   return (
-    <ListItem>
-      <div
-        className={cn('checkbox', { checked })}
-        onClick={() => {
-          onToggle(id);
-        }}
-      >
-        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
-        <div className="text">{text}</div>
+    <ListWrapper style={style}>
+      <div className="list-item">
+        <div
+          className={cn('checkbox', { checked })}
+          onClick={() => {
+            onToggle(id);
+          }}
+        >
+          {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+          <div className="text">{text}</div>
+        </div>
+        <div className="remove">
+          <MdRemoveCircleOutline onClick={() => onRemove(id)} />
+        </div>
       </div>
-      <div className="remove">
-        <MdRemoveCircleOutline onClick={() => onRemove(id)} />
-      </div>
-    </ListItem>
+    </ListWrapper>
   );
 };
 
